b2c9651354123ba5bbeedb9cbd6b6bd5c0bbb0ac
Antonio Ivanovski <toteto@users.noreply.github.com>
2024-06-10 13:05:14 +0200

    install @jpmorganchase/onyx-ssi-sdk and create did

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const { KeyDIDMethod } = require("@jpmorganchase/onyx-ssi-sdk");
+
+async function main() {
+  const keyDidMethod = new KeyDIDMethod();
+  console.log("Creating a new DID...");
+  const did = await keyDidMethod.create();
+  console.log("Created did: ", did.did);
+}
+
+main();
